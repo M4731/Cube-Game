cda4d3032f40e72bacb96ae1344faca81bb6d719
Mîndruleanu Matei Daniel <mateimindruleanu@gmail.com>
2022-05-03 20:41:11 +0300

    obstacle + collisions
    obstacle + collisions

---
+++ Cube-Game/Assets/PlayerCollision.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    void OnCollisionEnter(){
+        Debug.Log("hit");
+    }
+}
