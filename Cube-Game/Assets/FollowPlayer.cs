a149057f63c64dc92ca6d6e032c571fbb8494b15
Mîndruleanu Matei Daniel <mateimindruleanu@gmail.com>
2022-05-03 05:29:02 +0300

    coms
    added coms for the FollowPlayer class

--- Cube-Game/Assets/FollowPlayer.cs
+++ Cube-Game/Assets/FollowPlayer.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public Transform Player;
-    public Vector3 CameraPosition;
+
+    //The position between the camera and the player
+    public Vector3 CameraDistance;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Player.position + CameraPosition;
+        //This script makes the camera follow the player while keeping a "CameraDistance"(XYZ coordinates) from the center of the player object
+        transform.position = Player.position + CameraDistance;
     }
 }
